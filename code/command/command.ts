a0791acb1c83d9ba64933e1f5f6848b264f4e536
chenxv <chx1122@126.com>
2020-05-08 16:41:18 +0800

    docs：添加命令模式

---
+++ code/command/command.ts
@@ -0,0 +1,49 @@
+namespace CommandPattern {
+  export abstract class Command {
+    public execute(): void {}
+  }
+
+  export class ConcreteCommand1 extends Command {
+    private receiver: Receiver
+    constructor(receiver: Receiver) {
+      super()
+      this.receiver = receiver
+    }
+    public execute(): void {
+      console.log("ConcreteCommand1")
+      this.receiver.action()
+    }
+  }
+
+  export class ConcreteCommand2 extends Command {
+    private receiver: Receiver
+    constructor(receiver: Receiver) {
+      super()
+      this.receiver = receiver
+    }
+    public execute(): void {
+      console.log("ConcreteCommand2")
+      this.receiver.action()
+    }
+  }
+
+  export class Invoker {
+    private commands: Command[]
+
+    constructor() {
+      this.commands = []
+    }
+
+    public storeAndExecute(cmd: Command) {
+      this.commands.push(cmd)
+      cmd.execute()
+    }
+  }
+
+  export class Receiver {
+    public action(): void {
+      console.log("action called!")
+    }
+  }
+}
+
---
+++ code/command/demo.ts
@@ -0,0 +1,9 @@
+/// <reference path="command.ts" />
+namespace CommandPattern {
+  let receiver: CommandPattern.Receiver = new CommandPattern.Receiver(),
+  command1: CommandPattern.Command  = new CommandPattern.ConcreteCommand1(receiver),
+  command2: CommandPattern.Command  = new CommandPattern.ConcreteCommand2(receiver),
+  invoker : CommandPattern.Invoker  = new CommandPattern.Invoker()
+  invoker.storeAndExecute(command1)
+  invoker.storeAndExecute(command2)
+}
