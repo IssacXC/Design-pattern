3d3605e873ff12cc9f56814d9acd36000081a69d
chenxv <chx1122@126.com>
2020-05-12 09:42:53 +0800

    docs：添加迭代器模式demo

---
+++ code/iterator/demo.ts
@@ -0,0 +1,10 @@
+/// <reference path="iterator.ts" />
+namespace IteratorPattern {
+  let nArray = [1, 7, 21, 657, 3, 2, 765, 13, 65],
+  numbers: IteratorPattern.Numbers = new IteratorPattern.Numbers(nArray),
+  it: IteratorPattern.ConcreteIterator = <IteratorPattern.ConcreteIterator>numbers.createIterator()
+
+  while (it.hasNext()) {
+    console.log(it.next())
+  }
+}
